b38c99ded906ac8046bd4ac251639bed449a2b43
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-11-11 09:10:58 -0500

    print email adress

--- js/script.js
+++ js/script.js
@@ -1,21 +1,33 @@
-// **Descrizione:**
+
 // Attraverso l’apposita API di Boolean
 // https://flynn.boolean.careers/exercises/api/random/mail
 // generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
 // **Bonus**
 // Creare un loading e far comparire gli indirizzi email solamente quando sono stati TUTTI generati.
-// :+1::carnagione-3::+1:
-// 3
+
 
 const { createApp } = Vue
 
 createApp({
   data() {
     return {
-
+      email: ''
+    }
+  },
+  methods: {
+    getApi() {
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail?title=numero')
+      .then( risultato => {
+        console.log(risultato.data);
+        this.email = risultato.data.response;
+      })
+      .catch( error =>  {
+        console.log(error);
+      })
     }
   },
   mounted(){
-    console.log('montata')
+    this.getApi();
+    console.log('montata');
   }
 }).mount('#app')
